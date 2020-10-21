616b87c309623620bfc58a0ad37f21b85e30a7d4
Thibault MAUDET <41793338+thibaultmaudet@users.noreply.github.com>
2020-10-21 16:01:29 +0200

    Remove unsed components.

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { DevisComponent } from './devis/devis.component';
 import { GestionComponent } from './gestion/gestion.component';
 import { ListeComponent } from './liste/liste.component';
@@ -19,8 +17,6 @@ import { VoirClientComponent } from './voir-client/voir-client.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     DevisComponent,
     GestionComponent,
     ListeComponent,
@@ -33,8 +29,6 @@ import { VoirClientComponent } from './voir-client/voir-client.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'devis', component: DevisComponent },
       { path: 'gestion', component: GestionComponent },
       { path: 'liste', component: ListeComponent },
